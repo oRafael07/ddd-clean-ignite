44e46b6df97e2789e4707eca51cf3cc59adb97bd
Rafael Rocha <rafadeveloper.contato@gmail.com>
2023-05-05 11:26:29 -0400

    feat: add path alias and global type vitest

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,6 +1,6 @@
-import { UniqueEntityID } from './../../core/entities/unique-entity-id';
-import { Entity } from "../../core/entities/entity"
-import { Optional } from '../../core/types/optional';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { Entity } from "@/core/entities/entity"
+import { Optional } from '@/core/types/optional';
 
 interface AnswerProps {
   authorId: UniqueEntityID
