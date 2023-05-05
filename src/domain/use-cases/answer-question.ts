f2691b5ca6e23b2ec008ed6cb3aa2756c8251576
Rafael Rocha <rafadeveloper.contato@gmail.com>
2023-05-05 10:45:35 -0400

    feat: abstract to create a entity

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,5 +1,6 @@
 import { AnswersRepository } from './../repositories/answers-repository';
 import { Answer } from "../entities/answer"
+import { UniqueEntityID } from '../../core/entities/unique-entity-id';
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
@@ -10,10 +11,10 @@ interface AnswerQuestionUseCaseRequest {
 export class AnswerQuestionUseCase {
   constructor(private readonly answersRepository: AnswersRepository) {}
   async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({
-      content,
-      authorId: instructorId,
-      questionId
+    const answer = Answer.create({
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+      content
     })
 
     await this.answersRepository.create(answer)
