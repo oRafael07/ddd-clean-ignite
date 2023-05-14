94fa51af8ccf18532414752504ba3903a3401ae0
Rafael <rafadeveloper.contato@gmail.com>
2023-05-14 17:31:15 -0300

    feat: add use case comment on answer

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,37 @@
+import { InMemoryAnswersRepository  } from 'test/repositories/in-memory-answer-respository'
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswersCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+
+let inMemoryAnswerRepository: InMemoryAnswersRepository
+let inMemoryAnswerCommentsRepository: InMemoryAnswersCommentsRepository
+let sut: CommentOnAnswerUseCase
+
+describe('Comment on Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswerRepository = new InMemoryAnswersRepository()
+    inMemoryAnswerCommentsRepository =
+      new InMemoryAnswersCommentsRepository()
+
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswerRepository,
+      inMemoryAnswerCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on answer', async () => {
+    const answer = makeAnswer()
+
+    await inMemoryAnswerRepository.create(answer)
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      content: 'Comentário teste',
+    })
+
+    expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual(
+      'Comentário teste',
+    )
+  })
+})
